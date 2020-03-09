7028b97a5fabb04019a4b1e289cd13d4ea7e77c3
laveosa <laveosa@yahoo.com>
2020-03-09 18:44:31 +0200

    add - api: POST new user

--- app.js
+++ app.js
@@ -3,14 +3,17 @@ const express = require("express");
 const path = require("path");
 const dotenv = require("dotenv").config();
 const fs = require("fs");
-const routes = require("./app/server/routes/api/members");
+const bodyParser = require("body-parser");
+const routUser = require("./app/server/routes/api/members");
 
 const server = express();
 const PORT = process.env.PORT || 3000;
 
 server.set("view engine", "ejs");
 server.use(express.static(path.join(__dirname, "app")));
-server.use("/api/users", routes);
+server.use(bodyParser.urlencoded({ extended: false }));
+server.use(bodyParser.json());
+server.use("/api/users", routUser);
 
 server.get("/", (req, res) => {
   const _path = path.join(__dirname, "app/server/views/index.ejs");
--- app/server/routes/api/members.js
+++ app/server/routes/api/members.js
@@ -2,15 +2,32 @@ const express = require("express");
 const router = express.Router();
 const fs = require("fs");
 const path = require("path");
+const uuid = require("uuid");
+
+const validation = {
+  isText: function(cont) {
+    if (!cont || cont.length <= 0) {
+      return false;
+    }
+
+    return true;
+  },
+  isEmail: function(cont) {
+    if (!cont || cont.length <= 0) {
+      return false;
+    }
+
+    // var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    // return re.test(String(email).toLowerCase());
+
+    return true;
+  }
+};
 
 router.get("/", (req, res) => {
-  fs.readFile(
-    path.join(__dirname, "app/server/files/elements.json"),
-    "utf8",
-    (err, data) => {
-      res.json(JSON.parse(data));
-    }
-  );
+  fs.readFile("app/server/files/elements.json", "utf8", (err, data) => {
+    res.json(JSON.parse(data));
+  });
 });
 
 router.get("/:id", (req, res) => {
@@ -27,6 +44,29 @@ router.get("/:id", (req, res) => {
         message: "There is no such user ID"
       });
     }
+  });
+});
+
+router.post("/", (req, res) => {
+  if (!validation.isText(req.body.name)) {
+    return res.status(400).json({ message: "please enter correct name" });
+  }
+
+  let newUser = {
+    id: uuid.v4(),
+    name: req.body.name,
+    age: req.body.age,
+    position: req.body.position
+  };
+
+  fs.readFile("app/server/files/elements.json", "utf8", (err, data) => {
+    data = JSON.parse(data);
+    data.push(newUser);
+    data = JSON.stringify(data);
+
+    fs.writeFile("app/server/files/elements.json", data, err => {
+      res.json(JSON.parse(data));
+    });
   });
 });
 
